bc7dbb4c1ba612292c7c30c56abb0a01404f4919
Tamer Alssaleh (Paradoxial) <52636947+Paradoxial8172@users.noreply.github.com>
2024-04-17 21:55:10 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,12 +1,26 @@
 from tinytag import TinyTag
 import tkinter
+import tkinter.filedialog as fd
 from PIL import Image, ImageTk
 import io
+import pygame
+import os
 
 def exit():
     root.destroy()
 
-def get_track_information(filename) -> dict:
+pygame.mixer.init()
+
+root = tkinter.Tk()
+root.geometry("500x500")
+
+def open_file():
+    file = fd.askopenfilename()
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    return os.path.abspath(file)
+
+def get_track_information(filename):
 
     filename = filename.replace("\\", "/")
     track = TinyTag.get(filename)
@@ -20,46 +34,45 @@ def get_track_information(filename) -> dict:
     seconds = int((track_duration - hours * 3600 - minutes * 60))
     readable_duration = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
+    album_img_bytes_str = TinyTag.get(filename, image=True)
+
+    if album_img_bytes_str.get_image() is None:
+        image = ImageTk.PhotoImage(Image.open("no_album_cover.png"))
+    else:
+        img = Image.open(io.BytesIO(album_img_bytes_str.get_image())).resize((128,128))
+
+        image = ImageTk.PhotoImage(img)
+
     info = {
+        "Path": filename,
         "Title": title,
         "Artist": artist,
         "Album": album,
-        "Duration": readable_duration
+        "Duration": readable_duration,
+        "Album_Img_Bytes": album_img_bytes_str.get_image()
     }
 
-    return info
+    lbl1.configure(text=f"Title: {info['Title']}")
+    lbl2.configure(text=f"Artist: {info['Artist']}")
+    lbl3.configure(text=f"Album: {info['Album']}")
+    lbl4.configure(text=f"Duration: {info['Duration']}")
+    lbl5.configure(text=f"Path: {info['Path']}")
+    lbl6.configure(image=image)
 
-user_input = input("Enter path of audio file:\n").strip('"')
+    lbl6.image = image
 
+image = ImageTk.PhotoImage(Image.open("no_album_cover.png"))
 
+lbl1 = tkinter.Label(root, text=f"Title: {None}")
+lbl2 = tkinter.Label(root, text=f"Artist: {None}")
+lbl3 = tkinter.Label(root, text=f"Album: {None}")
+lbl4 = tkinter.Label(root, text=f"Duration: {None}")
+lbl5 = tkinter.Label(root, text=f"Path: {None}", wraplength=300)
+lbl6 = tkinter.Label(root, image=image)
 
-root = tkinter.Tk()
-root.geometry("500x500")
+lbl6.image = image
 
-track_info = get_track_information(user_input)
-
-album_img_bytes_str = TinyTag.get(user_input.replace("\\", "/"), image=True)
-
-b = album_img_bytes_str.get_image()
-try:
-    image = Image.open(io.BytesIO(b))
-    img = image.resize((128, 128))
-    album_img = ImageTk.PhotoImage(img)
-    
-except:
-    album_img = None
-
-lbl1 = tkinter.Label(root, text=f"Title: {track_info['Title']}")
-lbl2 = tkinter.Label(root, text=f"Artist/Band: {track_info['Artist']}")
-lbl3 = tkinter.Label(root, text=f"Album: {track_info['Album']}")
-lbl4 = tkinter.Label(root, text=f"Duration: {track_info['Duration']}")
-lbl5 = tkinter.Label(root, text="Album Cover:")
-
-if album_img is None:
-    no_album_image = ImageTk.PhotoImage(Image.open("images/no_album_cover.png"))
-    lbl6 = tkinter.Label(root, image=no_album_image)
-else:
-    lbl6 = tkinter.Label(root, image=album_img)
+b1 = tkinter.Button(root, text="Play", command=lambda: get_track_information(open_file()))
 
 lbl1.pack()
 lbl2.pack()
@@ -67,6 +80,8 @@ lbl3.pack()
 lbl4.pack()
 lbl5.pack()
 lbl6.pack()
+
+b1.pack()
 
 root.protocol("WM_DELETE_WINDOW", exit)
 
